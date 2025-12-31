e502495e7e9df732ae606ff0e04a784efa05f555
SamuelWerlang <samuelwerlang13@gmail.com>
2025-12-31 17:12:55 -0300

    new chars accepted

--- src/lexer.c
+++ src/lexer.c
@@ -12,6 +12,12 @@ typedef enum TokenType {
     TOKEN_OPEN_PAR,
     TOKEN_CLOSE_PAR,
     TOKEN_SEMICOL,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_DOT,
+    TOKEN_COMMA,
+    TOKEN_EXCLAMATION,
     TOKEN_INT,
     TOKEN_RET,
 } TokenType;
@@ -29,7 +35,7 @@ static void scan_token (
     size_t *count, 
     int *line
 );
-
+static void add_lexeme(unsigned char ch, Token *Token_list, size_t *count, int* line, TokenType tk_type);
 Token* Scanner(const char *src, size_t capacity, size_t *out_count);
 
 int main(void) {
@@ -41,8 +47,10 @@ int main(void) {
     }
     
     for (size_t i = 0; i < counter; i++) {
-        printf("Token Type: %d\n, lexeme: %s\n, line: %d", 
-        tokens[i].type, tokens[i].lexeme, tokens[i].line);
+        printf("Token Type: %d, lexeme: %s, line: %d\n", 
+        tokens[i].type, 
+        tokens[i].lexeme, 
+        tokens[i].line);
     }
 
     
@@ -54,6 +62,20 @@ int main(void) {
     return 0;
 }
 
+static void add_lex(unsigned char ch, Token *Token_list, size_t *count, int* line, TokenType tk_type) {
+
+    char *keywords[5] = {"int", "for", "while", "if", "else"};
+    char *lex = malloc(sizeof(char) * 2);
+                lex[0] = ch;
+                lex[1] = '\0';
+
+                Token_list[(*count)++] = (Token){
+                    .type = tk_type, 
+                    .lexeme = lex,
+                    .line = *line
+                };
+}
+
 static void scan_token (
     unsigned char ch, 
     Token *Token_list, 
@@ -61,24 +83,56 @@ static void scan_token (
     size_t *count, 
     int *line
 ) {
-
+    if (*count >=capacity) {
+        fprintf(stderr, "Token capacity exceeded");
+        return;   
+    }
     if (isspace(ch)) {
         if (ch == '\n') (*line)++ ;
-    return;
-}
+        return;
+    }
     switch (ch) {
         case '(': {
-                char *lex = malloc(sizeof(char) * 2);
-                lex[0] = '(';
-                lex[1] = '\0';
-
-                Token_list[(*count)++] = (Token){
-                    .type = TOKEN_OPEN_PAR, 
-                    .lexeme = lex,
-                    .line = *line
-                };
-               break;  
+            add_lex(ch, Token_list, count, line, TOKEN_OPEN_PAR);
+            break;  
         }
+        case ')': {
+            add_lex(ch, Token_list, count, line, TOKEN_CLOSE_PAR);
+            break;
+        }
+        case '{': {
+            add_lex(ch, Token_list, count, line, TOKEN_OPEN_BRACE);
+            break;
+        } 
+        case '}': {
+            add_lex(ch, Token_list, count, line, TOKEN_CLOSE_BRACE);
+            break;
+        }
+        case ';': {
+            add_lex(ch, Token_list, count, line, TOKEN_SEMICOL);
+            break;
+        }
+        case '.': {
+            add_lex(ch, Token_list, count, line, TOKEN_DOT);
+            break;
+        }
+        case '+': {
+            add_lex(ch, Token_list, count, line, TOKEN_PLUS);
+            break;
+        }
+        case '-': {
+            add_lex(ch, Token_list, count, line, TOKEN_MINUS);
+            break;
+        }
+        case '!': {
+            add_lex(ch, Token_list, count, line, TOKEN_EXCLAMATION);
+            break;
+        }
+         case ',': {
+            add_lex(ch, Token_list, count, line, TOKEN_COMMA);
+            break;
+        }
+
     }
 }
 
